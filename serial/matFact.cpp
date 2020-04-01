8107fc4374adc4bf09ce88e76fd9f9191e44f201
Petros Tzathas <tzathaspet@gmail.com>
2020-04-01 14:58:07 +0100

    Cache opt works a little better for serial too

--- serial/matFact.cpp
+++ serial/matFact.cpp
@@ -3,6 +3,33 @@
 #include <fstream>
 #include <iostream>
 
+constexpr int cache_line = 64 / sizeof(double);
+
+
+/** Sparse matrix type
+ */
+class sparse_matrix
+{
+public:
+    sparse_matrix(int n_rows, int n_columns)
+    : n_rows(n_rows), n_columns(n_columns)
+    {}
+
+    void add_element(int row, int column, double rating)
+    {
+        elements.push_back({row, column, rating});
+    }
+
+    struct entry_t
+    {
+        int row;
+        int col;
+        double rating;
+    };
+    
+    int n_rows, n_columns;
+    std::vector<entry_t> elements;
+};
 
 
 /** Matrix type
@@ -10,23 +37,28 @@
 class matrix
 {
 public:
-    using row_t = std::vector<double>;
-    
     matrix(int n_rows, int n_columns)
-    : n_rows(n_rows), n_columns(n_columns), data(n_rows, row_t(n_columns))
+    : n_rows(n_rows), n_columns(n_columns),
+      row_size((n_columns / cache_line + 1) * cache_line),
+      data(n_rows * row_size)
     {}
     
-    row_t& operator[](int row) { return data[row]; }
-    
-    const row_t& operator[](int row) const { return data[row]; }
-
-    matrix& operator=(matrix& m)
+    matrix(const sparse_matrix& m)
+    : n_rows(m.n_rows), n_columns(m.n_columns),
+      row_size((n_columns / cache_line + 1) * cache_line),
+      data(n_rows * row_size, 0.f)
     {
-        for (int r = 0; r < n_rows; r++)
-            for (int c = 0; c < n_columns; c++)
-                (*this)[r][c] = m[r][c];
-        
-        return *this;
+        for (auto& entry : m.elements) (*this)(entry.row, entry.col) = entry.rating;
+    }
+
+    double& operator()(int row, int col) { return data[row * row_size + col]; }
+    
+    double operator()(int row, int col) const { return data[row * row_size + col]; }
+
+    matrix& operator=(const matrix& m) 
+    {
+        for (int i = 0; i < data.size(); i++)
+            this->data[i] = m.data[i];
     }
 
     friend std::ostream& operator<<(std::ostream& s, const matrix& m)
@@ -34,7 +66,7 @@ public:
         for (int r = 0; r < m.n_rows; r++)
         {
             for (int c = 0; c < m.n_columns; c++)
-                s << m[r][c] << '\t';
+                s << m(r, c) << '\t';
             s << '\n';
         }
         
@@ -43,32 +75,11 @@ public:
 
 public:
     int n_rows, n_columns;
+    int row_size;
     
 private:
-    std::vector<row_t> data;
+    std::vector<double> data;
 };
-
-
-/** Sparse matrix type
- */
-class sparse_matrix
-{
-public:
-    void add_element(int row, int column, double rating)
-    {
-        elements.push_back({row, column, rating});
-    }
-    
-    struct entry
-    {
-        int row;
-        int col;
-        double rating;
-    };
-    
-    std::vector<entry> elements;
-};
-
 
 // globals
 int n_iter;
@@ -96,7 +107,7 @@ sparse_matrix parse_file(const char* filename)
     file >> nF;
     file >> nU >> nI >> n_elems;
     
-    sparse_matrix A;
+    sparse_matrix A(nU, nI);
     for (int i = 0; i < n_elems; i++)
     {
         int row, col;
@@ -117,7 +128,7 @@ double B(int i, int j, const matrix& L, const matrix& Rt)
 {
     double elem = .0;
     for(int k = 0; k < nF; k++) {
-        elem += L[i][k]*Rt[j][k];
+        elem += L(i, k)*Rt(j, k);
     }
     return elem;
 }
@@ -133,11 +144,11 @@ void update_LR(const sparse_matrix& A, matrix& L, matrix& Rt, matrix& oldL, matr
         temp = entry.rating - B(entry.row, entry.col, oldL, oldRt);
         for (int k = 0; k < nF; k++)
         {
-            deltaL = -2 * temp * oldRt[entry.col][k];
-            L[entry.row][k] -= learning_rate * deltaL;
+            deltaL = -2 * temp * oldRt(entry.col, k);
+            L(entry.row, k) -= learning_rate * deltaL;
 
-            deltaR = -2 * temp * oldL[entry.row][k];
-            Rt[entry.col][k] -= learning_rate * deltaR;
+            deltaR = -2 * temp * oldL(entry.row, k);
+            Rt(entry.col, k) -= learning_rate * deltaR;
         }
     }
 }
@@ -146,20 +157,19 @@ void result(const sparse_matrix& A, const matrix& L, const matrix& Rt)
 {
     double max;
     int col;
-    std::cout << '\n';
     auto it = A.elements.begin();
     auto it_end = A.elements.end();
     
     for(int i = 0; i < nU; i++) {
         max = 0;
         col = 0;
-        bool finished_row = (it == it_end || it->row > nU);
+        bool finished_row = (it == it_end || it->row > i);
 
         for(int j = 0; j < nI; j++) {
             if (!finished_row && j == it->col)
             {
                 it++;
-                finished_row = (it == it_end || it->row > nU);
+                finished_row = (it == it_end || it->row > i);
                 continue;
             }
             
@@ -186,15 +196,20 @@ int main(int argc, char** argv)
     
     for (int r = 0; r < nU; r++)
         for (int c = 0; c < nF; c++)
-            L[r][c] = RAND01 / (double) nF;
+            L(r, c) = RAND01 / (double) nF;
 
     for (int r = 0; r < nF; r++)
         for (int c = 0; c < nI; c++)
-            Rt[c][r] = RAND01 / (double) nF;
+            Rt(c, r) = RAND01 / (double) nF;
     
     //std::cout << "L:\n" << L << "\nR:\n" << Rt;
-    
-    for (int i = 0; i < n_iter; i++) update_LR(A, L, Rt, oldL, oldRt);
+
+    //std::cout << matrix(A) << std::endl;
+
+    for (int i = 0; i < n_iter; i++)
+    {
+        update_LR(A, L, Rt, oldL, oldRt);
+    }
     
     //std::cout << "L:\n" << L << "\nR:\n" << Rt;
 
