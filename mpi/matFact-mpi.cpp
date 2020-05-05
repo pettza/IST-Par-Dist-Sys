9b3b14e0bf9ce418cbf1bd779368a0c291b16623
Petros Tzathas <tzathaspet@gmail.com>
2020-05-05 16:57:45 +0300

    Working implementation

--- mpi/matFact-mpi.cpp
+++ mpi/matFact-mpi.cpp
@@ -21,6 +21,9 @@ double learning_rate;
 } globals;
 
 int id, p;
+int* proc_rows = nullptr;
+int rows_per_proc;
+int extra_rows;
 
 /** Sparse matrix type
  */
@@ -77,7 +80,7 @@ public:
         return s;
     }
 	
-	friend void swap(matrix a, matrix b)
+	friend void swap(matrix& a, matrix& b)
 	{
 		std::swap(a.n_rows, b.n_rows);
 		std::swap(a.n_columns, b.n_columns);
@@ -92,14 +95,8 @@ public:
     std::vector<double> data;
 };
 
-struct sparse_data
-{
-	int row, col;
-	double elem;
-};
-
 // Creates a matrix from a file
-int parse_file_send_data(const char* filename, sparse_matrix& A)
+int parse_file_distribute_A(const char* filename, sparse_matrix& A)
 {
     std::ifstream file(filename);
     
@@ -116,16 +113,17 @@ int parse_file_send_data(const char* filename, sparse_matrix& A)
     file >> globals.nF;
     file >> globals.nU >> globals.nI >> n_elems;
 	
-	int rows_per_proc = globals.nU / p;
-	int extra_rows = globals.nU % p;
+	rows_per_proc = globals.nU / p;
+	extra_rows = globals.nU % p;
 	
 	int row = 0, col = 0;
     double elem;
-    std::vector<sparse_data> sendingData;
+    std::vector<sparse_matrix::entry_t> sendingData;
 	int n_rows = 0;
 	int rows_to_send = rows_per_proc + (extra_rows == 0 ? 0 : 1);
 	int id = 0;
-    for (int i = 0; i < n_elems; i++)
+    proc_rows[0] = rows_to_send;
+	for (int i = 0; i < n_elems; i++)
     {
 		int prev_row = row;
 		
@@ -142,13 +140,14 @@ int parse_file_send_data(const char* filename, sparse_matrix& A)
 					int size[] = {(int) sendingData.size(), rows_to_send};
 					
 					MPI_Send(size, 2, MPI_INT, id, SizeTag, MPI_COMM_WORLD);
-					if (size[0] != 0) MPI_Send(sendingData.data(), size[0] * sizeof(sparse_data), MPI_BYTE, id, DataTag, MPI_COMM_WORLD);
+					if (size[0] != 0) MPI_Send(sendingData.data(), size[0] * sizeof(sparse_matrix::entry_t), MPI_BYTE, id, DataTag, MPI_COMM_WORLD);
 				}
 				
 				sendingData.clear();
 				n_rows -= rows_to_send ;
 				id++;
 				rows_to_send = rows_per_proc + (id >= extra_rows ? 0 : 1);
+				proc_rows[id] = rows_to_send;
 			}
         }
 		
@@ -160,17 +159,95 @@ int parse_file_send_data(const char* filename, sparse_matrix& A)
 	int size[] = {(int) sendingData.size(), rows_to_send};
 	
 	MPI_Send(size, 2, MPI_INT, id, SizeTag, MPI_COMM_WORLD);
-	if (size[0] != 0) MPI_Send(sendingData.data(), size[0] * sizeof(sparse_data), MPI_BYTE, id, DataTag, MPI_COMM_WORLD);
+	if (size[0] != 0) MPI_Send(sendingData.data(), size[0] * sizeof(sparse_matrix::entry_t), MPI_BYTE, id, DataTag, MPI_COMM_WORLD);
 	
 	id++;
 	size[0] = 0;
 	size[1] = 0;
-	for (; id < p; id++) MPI_Send(size, 2, MPI_INT, id, SizeTag, MPI_COMM_WORLD);
+	for (; id < p; id++)
+	{
+		proc_rows[id] = 0;
+		MPI_Send(size, 2, MPI_INT, id, SizeTag, MPI_COMM_WORLD);
+	}
 
     file.close();
     
-    return rows_per_proc + (extra_rows == 0 ? 0 : 1);
+    return proc_rows[0];
 }
+
+void initialize_distribute_LR(matrix& L, matrix& Rt, matrix& bufferL)
+{
+	#define RAND01 ((double) std::rand() / (double) RAND_MAX)
+
+	if (id == 0)
+	{
+		for (int i = 0; i < p; ++i)
+		{
+			for (int r = 0; r < proc_rows[i]; r++)
+				for (int c = 0; c < globals.nF; c++)
+					bufferL(r, c) = RAND01 / (double) globals.nF;
+				
+			if (i == 0) swap(L, bufferL);
+			else if (proc_rows[i] != 0) MPI_Send(bufferL.data.data(), proc_rows[i] * bufferL.row_size, MPI_DOUBLE, i, DataTag, MPI_COMM_WORLD);
+		}
+
+		for (int r = 0; r < globals.nF; r++)
+			for (int c = 0; c < globals.nI; c++)
+				Rt(c, r) = RAND01 / (double) globals.nF;
+	}
+	else if (L.data.size() != 0)
+	{
+		MPI_Status status;
+		MPI_Recv(L.data.data(), L.data.size(), MPI_DOUBLE, 0, DataTag, MPI_COMM_WORLD, &status);
+	}
+	
+	// Broadcast Rt
+	MPI_Bcast(Rt.data.data(), Rt.data.size(), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+}
+
+void exchangeRt(matrix& Rt, double* buffer)
+{
+	MPI_Request request;
+	MPI_Status status;
+	int size = Rt.n_rows * Rt.row_size;
+	
+	// Exchange messages in hypercube topology
+	for (int neighbor_mask = 0b1; neighbor_mask < p; neighbor_mask <<= 1)
+	{
+		int neighbor_id = (id & ~neighbor_mask) | (~id & neighbor_mask);
+
+		MPI_Isend(Rt.data.data(), size, MPI_DOUBLE, neighbor_id, DataTag, MPI_COMM_WORLD, &request);
+		MPI_Recv(buffer, size, MPI_DOUBLE, neighbor_id, DataTag, MPI_COMM_WORLD, &status);
+		MPI_Wait(&request, &status);
+		
+		// Update Rt before sending to next naighbor
+		for (int i = 0; i < size; ++i) Rt.data[i] += buffer[i];
+	}
+}
+
+void gatherL(matrix& FullL, matrix& L)
+{	
+	int* proc_sizes;
+	int* proc_offsets;
+
+	if (id == 0)
+	{
+		proc_sizes = new int[p];
+		proc_offsets = new int[p];
+		
+		proc_sizes[0] = L.n_rows * L.row_size;
+		proc_offsets[0] = 0;
+		for (int i = 1; i < p; ++i)
+		{
+			proc_sizes[i] = proc_rows[i] * L.row_size;
+			proc_offsets[i] = proc_sizes[i-1] + proc_offsets[i-1];
+		}
+	}
+	
+	int size = L.n_rows * L.row_size;
+	MPI_Gatherv(L.data.data(), size, MPI_DOUBLE, FullL.data.data(), proc_sizes, proc_offsets, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+}
+
 
 // Calculate B_ij
 double B(int i, int j, const matrix& L, const matrix& Rt)
@@ -212,43 +289,6 @@ void update_LR(const sparse_matrix& A, matrix& L, matrix& Rt, matrix& oldL, matr
     }
 }
 
-void exchangeRt(matrix& Rt, double* buffer)
-{
-	MPI_Request request;
-	MPI_Status status;
-	int size = Rt.n_rows * Rt.row_size;
-	
-	for (int neighbor_mask = 0b1; neighbor_mask < p; neighbor_mask <<= 1)
-	{
-		int neighbor_id = (id & ~neighbor_mask) | (~id & neighbor_mask);
-
-		MPI_Isend(Rt.data.data(), size, MPI_DOUBLE, neighbor_id, DataTag, MPI_COMM_WORLD, &request);
-		MPI_Recv(buffer, size, MPI_DOUBLE, neighbor_id, DataTag, MPI_COMM_WORLD, &status);
-		MPI_Wait(&request, &status);
-		
-		for (int i = 0; i < size; ++i) Rt.data[i] += buffer[i];
-	}
-}
-
-void gatherL(matrix& FullL, matrix& L)
-{
-	int* proc_rows = new int[p];
-	int* proc_offsets = new int[p];
-	int rows_per_proc = globals.nU / p;
-	int extra_rows = globals.nU % p;
-	
-	proc_rows[0] = (rows_per_proc + (extra_rows == 0 ? 0 : 1)) * L.row_size;
-	proc_offsets[0] = 0;
-	for (int id = 1; id < p; ++id)
-	{
-		proc_rows[id] = (rows_per_proc + (id >= extra_rows ? 0 : 1)) * L.row_size;
-		proc_offsets[id] = proc_rows[id-1] + proc_offsets[id-1];
-	}
-	
-	int size = L.n_rows * L.row_size;
-	MPI_Gatherv(L.data.data(), size, MPI_DOUBLE, FullL.data.data(), proc_rows, proc_offsets, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-}
-
 void result(const sparse_matrix& A, const matrix& L, const matrix& Rt)
 {
     double max;
@@ -281,15 +321,21 @@ void result(const sparse_matrix& A, const matrix& L, const matrix& Rt)
 
 int main(int argc, char* argv[])
 {
+	std::srand(0);
 	int n_rows;
 	sparse_matrix A;
+	double elapsed_time;
 	
 	MPI_Init(&argc, &argv);
+	
+	elapsed_time = -MPI_Wtime();
 	
 	MPI_Comm_rank(MPI_COMM_WORLD, &id);
     MPI_Comm_size(MPI_COMM_WORLD, &p);
 	
-	if (id == 0) n_rows = parse_file_send_data(argv[1], A);
+	proc_rows = new int[p];
+	
+	if (id == 0) n_rows = parse_file_distribute_A(argv[1], A);
 	else
 	{
 		MPI_Status status;
@@ -298,19 +344,19 @@ int main(int argc, char* argv[])
 		
 		n_rows = size[1];
 		
-		sparse_data* buffer;
+		sparse_matrix::entry_t* buffer;
 		
 		if (size[0] != 0)
 		{
-			buffer = new sparse_data[size[0]];
+			buffer = new sparse_matrix::entry_t[size[0]];
 			
-			MPI_Recv(buffer, size[0] * sizeof(sparse_data), MPI_BYTE, 0, DataTag, MPI_COMM_WORLD, &status);
+			MPI_Recv(buffer, size[0] * sizeof(sparse_matrix::entry_t), MPI_BYTE, 0, DataTag, MPI_COMM_WORLD, &status);
 			
 			int offset = buffer[0].row;
 			
 			for (int i = 0; i < size[0]; ++i)
 			{
-				A.add_element(buffer[i].row - offset, buffer[i].col, buffer[i].elem);
+				A.add_element(buffer[i].row - offset, buffer[i].col, buffer[i].rating);
 			}
 			
 			delete[] buffer;
@@ -327,15 +373,7 @@ int main(int argc, char* argv[])
     matrix oldRt(globals.nI, globals.nF);
 	double* bufferRt = new double[Rt.n_rows * Rt.row_size];
 	
-	#define RAND01 ((double) std::rand() / (double) RAND_MAX)
-
-	for (int r = 0; r < n_rows; r++)
-		for (int c = 0; c < globals.nF; c++)
-			L(r, c) = RAND01 / (double) globals.nF;
-
-	for (int r = 0; r < globals.nF; r++)
-		for (int c = 0; c < globals.nI; c++)
-			Rt(c, r) = RAND01 / (double) globals.nF;
+	initialize_distribute_LR(L, Rt, oldL);
 	
 	for (int i = 0; i < globals.n_iter; i++)
 	{
@@ -346,15 +384,27 @@ int main(int argc, char* argv[])
 		for (int i = 0; i < Rt.n_rows; ++i)
 			for (int j = 0; j < Rt.n_columns; ++j)
 				Rt(i, j) += oldRt(i, j);
+			
+		for (int i = 0; i < L.n_rows; ++i)
+			for (int j = 0; j < L.n_columns; ++j)
+				L(i, j) += oldL(i, j);
 	}
 	
 	matrix FullL(globals.nU, globals.nF);
 	
 	gatherL(FullL, L);
 
-	if (id == 0) result(A, FullL, Rt);
+	if (id == 0)
+	{
+		//std::cout << FullL << "\n\n" << Rt << std::endl;
+		result(A, FullL, Rt);
+	}
 	
+	delete[] proc_rows;
 	delete[] bufferRt;
+	
+	elapsed_time += MPI_Wtime();
+	if (id == 0) std::cout << "Elapsed time: " << elapsed_time << std::endl;
 	
 	MPI_Finalize();
 	
